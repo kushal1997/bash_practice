ba0261443fc843d695f1908115cd625b6d77eace
kushal1997 <kushalrao103@gmail.com>
2025-04-27 05:36:34 +0000

    If else elif example

---
+++ file.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#name="Bharati"
+
+read -p "Enter your name: " name
+
+if [ "$name" == "Kushal"  ]; then
+	echo "Hi Kushal"
+elif [ "$name" == "Bharati"  ]; then
+	echo "Chorni"
+else
+	echo "Juhar ho juhar"
+fi
+
+
